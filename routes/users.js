9c8533a2d37daa23997c995ddd61f7d3228f0862
ShineByPupil <522705046@qq.com>
2024-05-22 00:13:28 +0800

    routes修改成唯一出口

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,6 @@
-var express = require('express');
-var router = express.Router();
+var router = require('../router');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/users', function (req, res, next) {
   res.send('respond with a resource');
 });
-
-module.exports = router;
